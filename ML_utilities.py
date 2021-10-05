a935435a0e9a68cd41d5d04e40e2b97ae02aac90
RichardMuri <rmuri@umassd.edu>
2021-10-04 21:02:38 -0400

    Updated assert to work with np arrays

--- ML_utilities.py
+++ ML_utilities.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 __anum = 0
 
@@ -6,9 +7,11 @@ __anum = 0
 def assert_size(tensor, expected_size):
     if expected_size is torch.Size():
         check_var = expected_size
+    elif isinstance(expected_size, np.ndarray):
+        check_var = expected_size.shape
     else:
         check_var = torch.Size(expected_size)
-    ts = tensor.size()
+    ts = tensor.shape
     astring = "Expected tensor of size {}, got: {}".format(check_var, ts)
     assert check_var == ts, astring
     if __name__ == "__main__":
@@ -19,6 +22,8 @@ def assert_size(tensor, expected_size):
 
 if __name__ == "__main__":
     tensor = torch.zeros(3, 3, 3)
+    myArray = np.zeros([3, 3, 3])
     expected_size = torch.Size([3, 3, 3])
     assert_size(tensor, expected_size)
+    assert_size(myArray, myArray.shape)
     assert_size(tensor, [3, 1, 1])
